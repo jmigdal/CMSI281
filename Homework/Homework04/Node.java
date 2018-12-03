8ca6eaee0d2affc591a30b4f107be7d2b4ad69d1
jmigdal <35497548+jmigdal@users.noreply.github.com>
2018-12-03 11:07:02 -0800

    Completed program

---
+++ Homework/Homework04/Node.java
@@ -0,0 +1,22 @@
+/**
+ * A class to build a basic node. 
+ * @date November 26, 2018
+ * @author Kathryn Cole and James Migdal 
+ */
+
+public class Node {
+    private int data;
+
+    public Node( int key ) {
+        data = key;
+    }
+
+    public int getKey() {
+        return data;
+    }
+
+    public void setKey( int key ) {
+        data = key;
+    }
+
+}
